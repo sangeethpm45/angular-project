9235688978de6b7ca8ffeb5b0afcedb62bd7fb6b
sangeeth pm <sangeethpm45@gmail.com>
2021-04-11 19:43:50 +0530

    verthe

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,55 +9,20 @@ withCredentials:true
 })
 export class DataService {
   constructor(private http:HttpClient) { }
-//   accountdetails:any = {
-//     1000: {
-//         accno: 1000,
-//         name: "userone",
-//         balance: 6000,
-//         password: "user1",
-//     }, 
-//     1001: {
-//         accno: 1001,
-//         name: "usertwo",
-//         balance: 9000,
-//         password: "user2",
-//     },
-//     1002: {
-//         accno: 1002,
-//         name: "userthree",
-//         balance: 6000,
-//         password: "user3",
-//     },
-//     1003: {
-//         accno: 1003,
-//         name: "userfour",
-//         balance: 9000,
-//         password: "user4",
-//     },
-//     1004: {
-//         accno: 1004,
-//         name: "userfive",
-//         balance: 6000,
-//         password: "user5",
-//     },
-//     1005: {
-//         accno: 1005,
-//         name: "usersix",
-//         balance: 9000,
-//         password: "user6",
-//     }
-// };
+
 currentUser:any;
-//function to store data in local storage
+
+
+//function to store data in local storage---------------------------
 // saveDetails(){
 //   localStorage.setItem("accountdetails",JSON.stringify(this.accountdetails))
 //   if(this.currentUser){
 //     localStorage.setItem("currentUser",JSON.stringify(this.currentUser))
 //   }
 // }
-//----end--------
+//----end----------------------------------------------------------------
 
-//function to get data from local storage
+//function to get data from local storage---------------------------------
 // getDetails(){
 //   if(localStorage.getItem('accountdetails')){
 //     this.accountdetails=JSON.parse(localStorage.getItem("accountdetails")||'')
@@ -67,11 +32,12 @@ currentUser:any;
 
 //   }
 // }
-//-------end----
+//-------end--------------------------------------------------------------
 
 
 
-//-----Register a new user
+//-----Register a new user-------------------------------
+
 register(accno:any,name:any,password:any){
    const data={
     accno,
@@ -82,7 +48,8 @@ register(accno:any,name:any,password:any){
 return this.http.post('http://localhost:3000/register',data)
 
 }
-//--------------------------
+
+//----------------------------------------------------------
 
 
 
@@ -91,22 +58,7 @@ return this.http.post('http://localhost:3000/register',data)
 // Login function-----------------------------------------
 
 login(accn:any,passw:any){
-//   if (accn in this.accountdetails) {
-//     if (passw == this.accountdetails[accn]["password"]) {
-//         alert("Login Sucess")
-//       this.currentUser=this.accountdetails[accn]['name']
 
-//       this.saveDetails()
-//         return true
-        
-//     } else {
-//         alert("inavlid");
-//         return false
-//     }
-// } else {
-//     alert("no user");
-//     return false
-// }
 const data={
   accn,
   password:passw
@@ -114,11 +66,14 @@ const data={
 return this.http.post("http://localhost:3000/login",data,options)
  }
  
+//---------------------------------------------------------------------
+
+
 //---------------------------------------------------
 
 
 
-//---funtion to deposit money
+//---funtion to deposit money----------------------------------------
 
 deposit(accn:any,pwd:any,amd:any){
   var amt=parseInt(amd)
@@ -128,26 +83,11 @@ deposit(accn:any,pwd:any,amd:any){
     amt
   }
   return this.http.post("http://localhost:3000/deposit",data,options)
-
-//   this.getDetails()
-//   if (accn in this.accountdetails) {
-//     var usr=this.accountdetails[accn]["name"]
-//     if (pwd == this.accountdetails[accn]["password"]&&usr==this.currentUser) {
-        
-//       this.accountdetails[accn]['balance']+=amd
-//       alert("amount credited successfully new balance"+this.accountdetails[accn]['balance'])  
-//         this.saveDetails()
-//     } else {
-//         alert("inavlid");
-        
-//     }
-// } else {
-//     alert("no user");
-    
-// }
 }
 
 //-----------------------------------------------------------
+
+
 
 
 //----funtion to withdraw money-------------------------------------------
@@ -163,5 +103,9 @@ withdraw(accn:any,pass:any,amd:any){
  }
 //-----------------------------------------------------------------------
   
+
+
+
+
 }
  
